658451d7db5b1850ff7d4c1ead5e2e4451ded1b9
Kashyap_Aman_Singh <119684617+KashyapAmanSingh@users.noreply.github.com>
2023-08-19 15:02:26 +0530

    Style- Updated Css Sheet & Added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+
+
+
 import './App.css';
-import Searched from './Component/Searched';
+// import Searched from './Component/Searched';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import DetailShow from './Component/Detail';
+import DetailShow from './Component/Detail/Detail';
+
+
+import Home from './Component/Home';
+
 
 function App() {
   return (
     <>
+ 
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Searched />} />
+          <Route path="/" element={<Home />} />
+
           <Route path="/DetailShow/:id" element={<DetailShow />} />
+          
         </Routes>
       </BrowserRouter>
     </>
